b46681eaa50703af8c14a704f80c66b865839c95
rafafsi <rafaelafsinacio@gmail.com>
2022-02-17 09:20:38 -0300

    Developed person create operation

--- src/main/java/one/digitalinnovation/personapi/controller/PersonalController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonalController.java
@@ -1,29 +1,29 @@
 package one.digitalinnovation.personapi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import one.digitalinnovation.personapi.dto.MessageResponseDTO;
+import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonalController {
 
-    @GetMapping
-    public String getBook() {
-        return "API test!";
+
+    private PersonService personService;
+
+    @Autowired
+    public PersonalController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO createPerson(@RequestBody Person person) {
+        return personService.createPerson(person);
     }
 }
 
 
-/*
-*  @RestController - fica na classe principal (PersonalController -> porta de entrada do projeto)
-*           significa que é um controlador e que vai ser acessado através de uma API REST
-*
-*  @RequestMapping  - apresenta o caminho principal da API
-*  (que atende o nível 1 de maturidade de uma api: nome de recursos bem definidos)
-*   o java permite essa definição de recurso a nível de classe
-*
-*   api - é uma api
-*   v1 - versionamento
-*   people - classe que gerencia toda people
-* */
